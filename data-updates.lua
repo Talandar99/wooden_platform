4e649b62b145fe24fe1f28af0ccdf0c3f6d1fa03
Talandar <talandar66@protonmail.com>
2026-05-31 23:06:08 +0200

    lignumis compat

--- data-updates.lua
+++ data-updates.lua
@@ -9,9 +9,9 @@ if mods["pelagos"] then
 			},
 			enabled = false,
 			ingredients = {
-				{ type = "item", name = "wood", amount = 15 },
-				{ type = "item", name = "coconut-sealant", amount = 5 },
-				{ type = "item", name = "coconut-husk", amount = 10 },
+				{ type = "item", name = "wood", amount = 10 },
+				{ type = "item", name = "coconut-sealant", amount = 3 },
+				{ type = "item", name = "coconut-husk", amount = 15 },
 			},
 			results = { { type = "item", name = "wooden-platform", amount = 1 } },
 			auto_recycle = false,
@@ -28,6 +28,29 @@ if mods["pelagos"] then
 	data.raw["recipe"]["wooden-platform"].enabled = false
 end
 
+if mods["lignumis"] then
+	data:extend({
+		{
+			type = "recipe",
+			name = "lignumis-wooden-platform",
+			icons = {
+				{ icon = "__wooden_platform__/graphics/wooden-platform-icon.png", icon_size = 64 },
+				{ icon = data.raw["item"]["lumber"].icon, icon_size = 64, scale = 0.25, shift = { 8, 8 } },
+			},
+			enabled = false,
+			ingredients = {
+				{ type = "item", name = "lumber", amount = 30 },
+			},
+			results = { { type = "item", name = "wooden-platform", amount = 1 } },
+			auto_recycle = false,
+		},
+	})
+	table.insert(
+		data.raw["technology"]["wood-science-pack"].effects,
+		{ type = "unlock-recipe", recipe = "lignumis-wooden-platform" }
+	)
+end
+
 -- arig compatibility
 if mods["planetaris-arig"] then
 	local item = data.raw.item["wooden-platform"]
@@ -35,4 +58,19 @@ if mods["planetaris-arig"] then
 		table.insert(item.place_as_tile.tile_condition, "arig-sand")
 		table.insert(item.place_as_tile.tile_condition, "arig-deep-sand")
 	end
+end
+local function add_additional_recipe_category(recipe_name, category)
+	if not data.raw["recipe"][recipe_name] then
+		return
+	end
+	if not data.raw["recipe"][recipe_name].additional_categories then
+		data.raw["recipe"][recipe_name].additional_categories = { "wood-processing" }
+	end
+	table.insert(data.raw["recipe"][recipe_name].additional_categories, category)
+end
+
+if mods["lignumis"] then
+	add_additional_recipe_category("pelagos-wooden-platform", "wood-processing")
+	add_additional_recipe_category("lignumis-wooden-platform", "wood-processing")
+	add_additional_recipe_category("wooden-platform", "wood-processing")
 end
